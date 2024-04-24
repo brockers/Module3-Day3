3bb26fce2ca7351a984c04fc5101c776882fa008
Bobby Rockers <rrockers@gmail.com>
2024-04-24 18:20:40 -0500

    Show file system stuff

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import _ from 'lodash';
 import { color, log, red, green } from 'console-log-colors';
 
@@ -8,3 +9,14 @@ console.log(color.red(myarray));
 const output = _.uniq(myarray);
 
 console.log(output);
+
+// echo 'node_modules/*' > .gitignore
+// git add -A
+// git commit -m "Some message"
+
+// Sync File Access
+const file = fs.readFileSync('./data.json', {encoding: 'utf8'});
+console.log(color.red(file));
+
+// An ASync File Access method
+fs.readFile('./data.json', {encoding: 'utf8'}, (err,f) => console.log(color.green(f)));
