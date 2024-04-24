7235ce23d08ec09974560ee12c1246177acb5ff3
Bobby Rockers <rrockers@gmail.com>
2024-04-24 18:29:10 -0500

    Showing web stuff.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import _ from 'lodash';
-import { color, log, red, green } from 'console-log-colors';
+import { color, log, red, green, blue } from 'console-log-colors';
+import request from 'request';
 
 const myarray = [ 1, 2, 3, 4, 5, 8, 4, 3, 9 ];
 
@@ -20,3 +21,8 @@ console.log(color.red(file));
 
 // An ASync File Access method
 fs.readFile('./data.json', {encoding: 'utf8'}, (err,f) => console.log(color.green(f)));
+
+request('https://jsonplaceholder.typicode.com/posts', (err, resp, body) => {
+    const parsed = JSON.parse(body);
+    parsed.forEach( (x) => console.log(x));
+});
